c42a7f417d030e38a54ff5270195258537025f00
Rania345 <raniatriek@gmail.com>
2023-12-11 12:31:12 +0100

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
-export LD_PRELOAD=/tmp/libmask.so
+wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/101-win.so
+export LD_PRELOAD=/tmp/101-win.so
