cd9ea737ed828caa40ca5a71d9217a569eb2e279
Rania345 <raniatriek@gmail.com>
2023-12-11 12:39:15 +0100

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/101-win.so
+wget -P /tmp https://github.com/rania345/alx-low_level_programming/raw/master/0x18-dynamic_libraries/101-win.so
 export LD_PRELOAD=/tmp/101-win.so
